c7a59fe342c73bc8e1de879b156c84cb51f3d07b
Buck Doyle <b@chromatin.ca>
2019-07-04 19:23:08 -0500

    Update assertion to expect downcasing

--- ts/test/components/emoji/lib_test.ts
+++ ts/test/components/emoji/lib_test.ts
@@ -5,12 +5,12 @@ import {
 } from '../../../components/emoji/lib';
 
 describe('replaceColons', () => {
-  it('replaces known emoji between colons', () => {
+  it('replaces known downcased-emoji between colons', () => {
     const noEmoji = replaceColons('hello');
     assert.equal(noEmoji, 'hello');
 
-    const anEmoji = replaceColons('hello :grinning:');
-    assert.equal(anEmoji, 'hello 😀');
+    const anEmojiWithCaps = replaceColons('hello :Grinning:');
+    assert.equal(anEmojiWithCaps, 'hello 😀');
 
     const skinToneModifierEmoji = replaceColons('hello :wave::skin-tone-5:!');
     assert.equal(skinToneModifierEmoji, 'hello 👋🏿!');
